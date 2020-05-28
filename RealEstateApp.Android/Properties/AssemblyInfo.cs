678113343c3cdf30d7025d43c45679ac51e74b26
David Broadfoot <dbroadfoot@gmail.com>
2020-05-28 21:39:39 +1000

    Module 2 - After / Module 3 - Before

--- RealEstateApp.Android/Properties/AssemblyInfo.cs
+++ RealEstateApp.Android/Properties/AssemblyInfo.cs
@@ -15,6 +15,13 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
+#if !DEBUG
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
+#endif
 
 
 
